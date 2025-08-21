bd7bc7a9bef8248965e206100a7475c73488c138
Ege Hidayet Koca <kocaege777@gmail.com>
2025-08-21 10:03:00 +0300

    feat: add validation rules for filters

--- src/implementation-roadmap.js
+++ src/implementation-roadmap.js
@@ -38,3 +38,8 @@ export const stage8 = {
   "date": "2025-08-08",
   "message": "ci: add build and test workflow"
 };
+
+export const stage9 = {
+  "date": "2025-08-21",
+  "message": "feat: add validation rules for filters"
+};
